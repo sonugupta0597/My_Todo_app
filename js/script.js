13c8064347cd44d19103159a0a47febe0922ed8e
sonugupta0597 <47735150+sonugupta0597@users.noreply.github.com>
2020-08-13 23:47:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,10 +18,11 @@ function addList() {
     if (inputValue.length != 0) {
         var element = document.getElementById("addListCard");
         var childElement = document.createElement("div");
-        childElement.id = document.getElementsByClassName("card").length++;
+        var count = document.getElementsByClassName("card").length;
+        childElement.id = "card" + count + 1;
         childElement.classList.add("card");
         var currentdate = new Date();
-        childElement.innerHTML = '<h1 class="card-title">' + document.getElementById("addListItem-input").value + '</h1><span>' + '</span><div class="make-hide"><hr><div id="itemList" class="itemList" style="margin-top:20px"></div></div><div class="font"><i style="color: lightblue;" class="fa fa-plus-circle addTasks" aria-hidden="true" onclick="addItem()"></i></div>';
+        childElement.innerHTML = '<h1 class="card-title" id=' + "list" + (count + 1) + '>' + document.getElementById("addListItem-input").value + '</h1><div class="make-hide" id=' + "make-hide" + (count + 1) + '><hr><div id=' + "itemList" + (count + 1) + ' class="itemList" style="margin-top:20px"></div></div><div class="font"><i style="color: lightblue;" class="fa fa-plus-circle addTasks" aria-hidden="true" id="' + "icon" + (count + 1) + '"onclick="addItem(' + (count + 1) + ')"></i ></div>';
 
         element.prepend(childElement);
     }
@@ -42,23 +43,31 @@ function removeBlurEffect() {
 
 }
 
-function addItem() {
+function addItem(count) {
+    var element = document.createElement('span');
+    element.id = count;
+    element.style.display = "none";
     document.getElementById("addItem").style.display = "block";
+    document.getElementById("addItem").appendChild(element);
+
 }
 
 function addListItem() {
-    var element = document.createElement("p");
-    element.classList.add("item");
-    element.innerHTML = document.getElementById("addListItem-input1").value;
-    document.getElementById("itemList").prepend(element);
-    var span = document.createElement("span");
-    var complete = document.createTextNode("complete");
-    span.appendChild(complete);
-    span.style.borderRadius = "20%";
-    span.style.backgroundColor = "green";
-    span.setAttribute("onclick", "completeTask(this)");
-    element.appendChild(document.createElement("br"));
-    element.appendChild(span);
+    if (document.getElementById("addListItem-input1").value.length != 0) {
+        var element = document.createElement("p");
+        element.classList.add("item");
+        element.innerHTML = document.getElementById("addListItem-input1").value;
+        document.getElementById("itemList" + (document.getElementById("addItem").lastChild.id)).prepend(element);
+        var span = document.createElement("span");
+        var complete = document.createTextNode("complete");
+        span.appendChild(complete);
+        span.style.borderRadius = "20%";
+        span.style.backgroundColor = "green";
+        span.setAttribute("onclick", "completeTask(this)");
+        element.appendChild(document.createElement("br"));
+        element.appendChild(span);
+    }
+
     document.getElementById("addItem").style.display = "none";
 }
 
